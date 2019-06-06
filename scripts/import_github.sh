6380161b7c11ee9dba9a984367017de3c041a93b
romL69 <romain.loire@ynov.com>
2019-06-06 12:14:36 +0200

    modif table event row

--- scripts/import_github.sh
+++ scripts/import_github.sh
@@ -6,7 +6,7 @@ CURRENT_DIRECTORY=$CURRENT_DIRECTORY/..
 
 #Delete  files
 rm -Rf $CURRENT_DIRECTORY/imports
-rm -Rf $CURRENT_DIRECTORY/data
+rm -Rf $CURRENT_DIRECTORY/data.json
 
 #Télécharge les json compressés
 echo "Téléchargement des données en cours..."
@@ -18,7 +18,7 @@ gzip -d $CURRENT_DIRECTORY/imports/2017-03-07-{0..1}.json.gz
 echo "Décompression terminée."
 
 #Concatène les fichier json en un seul fichier
-cat $CURRENT_DIRECTORY/imports/* > $CURRENT_DIRECTORY/data
+cat $CURRENT_DIRECTORY/imports/* > $CURRENT_DIRECTORY/data.json
 
 #Supprime le dossier imports
 rm -Rf $CURRENT_DIRECTORY/imports
