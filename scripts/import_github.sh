d12574de55860b348e1ca9ada4f5f92f4bea44ce
Cedric Lesueur <cedric1lesueur@gmail.com>
2019-06-06 12:38:29 +0200

    CA MARCHE... pour de vrai

--- scripts/import_github.sh
+++ scripts/import_github.sh
@@ -10,11 +10,11 @@ rm -Rf $CURRENT_DIRECTORY/data.json
 
 #Télécharge les json compressés
 echo "Téléchargement des données en cours..."
-wget --quiet -P $CURRENT_DIRECTORY/imports https://data.gharchive.org/2017-03-07-{0..1}.json.gz
+wget --quiet -P $CURRENT_DIRECTORY/imports https://data.gharchive.org/2017-03-07-2.json.gz
 
 #Décompresse les dossiers json
 echo "Décompression des données en cours..."
-gzip -d $CURRENT_DIRECTORY/imports/2017-03-07-{0..1}.json.gz
+gzip -d $CURRENT_DIRECTORY/imports/2017-03-07-2.json.gz
 echo "Décompression terminée."
 
 #Concatène les fichier json en un seul fichier
