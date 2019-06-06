5e88602d597c51eaadb1b10c523dcba2334038f4
romL69 <romain.loire@ynov.com>
2019-06-06 09:25:21 +0200

    debut create user

---
+++ sql/create_users.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS super_admin;
+DROP ROLE IF EXISTS super_admin;
+DROP ROLE IF EXISTS gitevents;
+
+CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
+CREATE DATABASE super_admin OWNER super_admin;
+
+CREATE ROLE gitevents LOGIN PASSWORD '12345';
+ALTER DATABASE gitevents OWNER TO gitevents
