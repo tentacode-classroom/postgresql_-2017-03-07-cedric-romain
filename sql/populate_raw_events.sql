c265372ca8503103e493c8a7f056d838a1cae63b
Cedric Lesueur <cedric1lesueur@gmail.com>
2019-06-07 09:56:34 +0200

    modif populate raw events

--- sql/populate_raw_events.sql
+++ sql/populate_raw_events.sql
@@ -1,6 +1,9 @@
 --Se connecte à la bdd gitevents
 \connect gitevents
 
+--Supprime la table 'events_raw' si elle existe
+DROP TABLE IF EXISTS events_raw;
+
 --Créé une table 'events_raw' avec un champ 'id' et un champ 'data_json' de type jsonb
 CREATE TABLE events_raw (
        id serial PRIMARY KEY,
