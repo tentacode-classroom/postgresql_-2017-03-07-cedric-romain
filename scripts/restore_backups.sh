4d57c5d455faef68ecaef34d9885ba57a5cf2501
Cedric Lesueur <cedric1lesueur@gmail.com>
2019-06-16 22:29:06 +0200

    Modif script restore_backups.sh

--- scripts/restore_backups.sh
+++ scripts/restore_backups.sh
@@ -3,7 +3,11 @@
 #set CURRENT_DIRECTORY variable
 CURRENT_DIRECTORY=`dirname $0`
 CURRENT_DIRECTORY=$CURRENT_DIRECTORY/..
-
-#Restoration de gitevents et des roles
-psql --username=super_admin -f $CURRENT_DIRECTORY/backups/backup_gitevents.sql
-psql --username=super_admin -f $CURRENT_DIRECTORY/backups/backup_roles.sql
+if [ -d "$CURRENT_DIRECTORY/backups" ]; then
+    #Restoration de gitevents et des roles
+    psql --username=super_admin -f $CURRENT_DIRECTORY/backups/backup_gitevents.sql
+    psql --username=super_admin -f $CURRENT_DIRECTORY/backups/backup_roles.sql
+else
+    echo "No backups found";
+    echo "Please run ./save_backups.sh before using this command";
+fi
